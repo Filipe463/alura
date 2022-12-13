2bfb2bdd753a64cb2a5d1abf4d7d78d72a1ae165
Filipe463 <flipe463@gmail.com>
2022-12-13 14:28:51 +0000

    Rodando atraves do for

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -2,4 +2,7 @@
 
 CAMINHO_IMAGEM=~/alura/imagens-livros
 
-convert $CAMINHO_IMAGEM/$1.jpg $CAMINHO_IMAGEM/$1.png
+for imagem in $@
+	do
+		convert $CAMINHO_IMAGEM/$imagem.jpg $CAMINHO_IMAGEM/$imagem.png
+	done
