ede202bf5c77cf77d9358b9f59abb07dc974d7e4
Filipe463 <flipe463@gmail.com>
2023-02-22 10:45:47 -0300

    22-02-2023

--- Java Script para Web/Desafio telefone/main.js
+++ Java Script para Web/Desafio telefone/main.js
@@ -1,6 +1,20 @@
-document.querySelector('input[type=tel]');
-
 const listaDeTeclas = document.querySelectorAll('input[type=button]');
+const inputTel = document.querySelector('input[type=tel]');
 
-console.log(listaDeTeclas);
+for (indice = 0; indice < listaDeTeclas.length; indice++) {
 
+  const tecla = listaDeTeclas[indice];
+
+  tecla.onclick = function () {
+    inputTel.value = inputTel.value + tecla.value;
+
+  tecla.onkeydown = function(evento){
+    if (evento.code === "Enter" || evento.code === "Space")
+
+      {tecla.classList.add("ativa");}
+
+    tecla.onkeyup = function()
+    {tecla.classList.remove('ativa');}
+  }
+  }
+}
--- Java Script para Web/aluramidi/main.js
+++ Java Script para Web/aluramidi/main.js
@@ -1,8 +1,65 @@
-function tocaSomPom(){
-	document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio){
+	const elemento = document.querySelector(seletorAudio);
+
+
+	if(elemento != null && elemento.localName === 'audio'){
+	
+			elemento.play();
+		}
+	else{
+		alert('Elemento não encotrado ou seletor não encotrado');
+	}
+	
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
-listaDeTeclas[0].onclick = tocaSomPom;
+/* Alura mid feito While
+let contador =  0;
+
+while(contador < listaDeTeclas.length){
+
+	const tecla = listaDeTeclas[contador];
+	const instrumento = tecla.classList[1];
+	const  idAudio = `#som_${instrumento}`;
+
+	tecla.onclick = function(){
+		tocaSom(idAudio);
+	}
+	contador = contador + 1;
+	
+}
+
+*/
+//Alura Midi feito com FOR
+
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+
+	const tecla = listaDeTeclas[contador];
+	const instrumento = tecla.classList[1];
+	const  idAudio = `#som_${instrumento}`;
+
+	tecla.onclick = function(){
+		tocaSom(idAudio);
+	}
+ 
+ 
+	tecla.onkeydown = function(evento){
+		
+		if (evento.code === 'Space' || evento.code === 'Enter' || evento.code === "NumpadEnter")
+			{tecla.classList.add('ativa')
+			};
+		}
+	
+
+	tecla.onkeyup = function (){
+		tecla.classList.remove('ativa');
+	}
+	}
+
+		
+
+
+
